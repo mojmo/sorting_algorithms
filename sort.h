54f920ea66bbd08a7faf5dc33315be74cba8b5c3
mojtaba <mojtaba.mohamed187@gmail.com>
2023-12-16 23:19:58 +0200

    add new functions prototypes

--- sort.h
+++ sort.h
@@ -28,5 +28,8 @@ void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void perform_quicksort(int *array, int start, int end, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes_forward(listint_t **head, listint_t **tail, listint_t **curr);
+void swap_nodes_backward(listint_t **head, listint_t **tail, listint_t **curr);
 
 #endif /* SORT_H */
