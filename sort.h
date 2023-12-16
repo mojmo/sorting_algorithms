c285fffbf92a88f63124f74e8883ab442b91064e
mojtaba <mojtaba.mohamed187@gmail.com>
2023-12-16 02:54:05 +0200

    add new functions prototypes

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -25,5 +27,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int start, int end, size_t size);
 void perform_quicksort(int *array, int start, int end, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
